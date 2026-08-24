2dd57ebd46fa21a685895128389e84d06693fd7a
Ahmed Sameh <ahmedsaameh14@gmail.com>
2026-08-24 12:50:04 +0300

    Fix model bug

--- models/admin.model.js
+++ models/admin.model.js
@@ -1,29 +1,30 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 
-const adminSchema = new mongoose.Schema({
+const adminSchema = new mongoose.Schema(
+  {
     email: {
-    type: String,
-    require: true,
-    unique: true,
-    match: [/^\S+@\S+\.\S+$/, "Please use a valid email address"],
+      type: String,
+      required: true,
+      unique: true,
+      match: [/^\S+@\S+\.\S+$/, "Please use a valid email address"],
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 6,
+    },
   },
-  password: {
-    type: String,
-    require: true,
-    minlength: 6
-  }
-},
-{ timestamps:true });
+  { timestamps: true }
+);
 
-adminSchema.pre('save' , async function(next){
-    if (!this.isModified('password')) return next();
-    this.password = await bcrypt.hash(this.password , 10);      
-    next();
-})
+adminSchema.pre('save', async function () {
+  if (!this.isModified('password')) return;
+  this.password = await bcrypt.hash(this.password, 10);
+});
 
-adminSchema.methods.correctPassword = async function(inputPassword){      
-    return await bcrypt.compare(inputPassword , this.password);
-}
+adminSchema.methods.correctPassword = async function (inputPassword) {
+  return await bcrypt.compare(inputPassword, this.password);
+};
 
 module.exports = mongoose.model('Admin', adminSchema);
